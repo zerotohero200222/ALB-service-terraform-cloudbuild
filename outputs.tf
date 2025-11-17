7cee8498f963234b35f19580257b8124fb894c5a
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 08:03:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@ output "lb_ip" {
   value = google_compute_global_address.lb_ip.address
 }
 
-output "url_map" {
-  value = google_compute_url_map.lb_urlmap.self_link
+output "ssl_certificate_status" {
+  value = google_compute_managed_ssl_certificate.lb_cert.managed[0].status
 }
 
-output "https_proxy" {
-  value = google_compute_target_https_proxy.lb_proxy.self_link
+output "url_map" {
+  value = google_compute_url_map.lb_urlmap.self_link
 }
 
 output "forwarding_rule" {
